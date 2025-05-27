0b1052c182fc6d9ac4790780ad7d98b510e97972
DoxB <jg.doxb@gmail.com>
2025-05-27 17:19:32 +0900

    Switch Springboot

---
+++ src/main/java/com/example/earthweb/EarthWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.earthweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthWebApplication.class, args);
+    }
+
+}
